6f118bf46520a04c2787173559539ad9d758d707
tetraxcode <abhinavreddy1961@gmail.com>
2022-10-17 01:30:08 -0700

    added static root in settings.py

--- Zytools_django/settings.py
+++ Zytools_django/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/home/pba/PBA-Django/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

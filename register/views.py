9efdb56fea9a6a4ef5ee5692b530841d564726cb
tetraxcode <abhinavreddy1961@gmail.com>
2022-12-11 11:55:09 -0800

    Added login efailed

--- register/views.py
+++ register/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from markupsafe import re
 from .forms import RegisterForm
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.models import User
 # Create your views here.
 
 def register(response):
@@ -16,6 +17,7 @@ def register(response):
 
 def login(request):
     if request.method == "POST":
+        print(request.POST)
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
@@ -24,6 +26,16 @@ def login(request):
             auth_login(request, user)
             return redirect('/')
         else:
-            return render(request, 'login/login.html', {'invalid': 'Invalid username/password'})
+            return render(request, 'login/login.html', {'message': 'Invalid username/password'})
     # return render(request, 'todo/login.html', {'form': form})
     return render(request, 'login/login.html')
+
+def forgot_password(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        user_exist = User.objects.filter(email=email).exists()
+        if user_exist:
+            pass
+        else:
+            return render(request, 'register/forgot.html', {'message': "Email does not exist, please register to use this application"})
+    return render(request, 'register/forgot.html')
